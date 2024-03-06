8846776ea927b618dc259f9fac6d0fbd24cc55b3
kokanevina <kokane.vina@gmail.com>
2024-03-06 16:02:45 +0530

    commit

--- Typescript Workspace/Inheritance/OverridingDemo.ts
+++ Typescript Workspace/Inheritance/OverridingDemo.ts
@@ -6,12 +6,32 @@ class NeoEmployee{
     protected experience:number;
     getGrossSalary():number{
         // ta : 5%, da: 10% hra 12 %
+        console.log("in NeoEmployee method");     
         return this.basicSalary+0.05*this.basicSalary+0.1*this.basicSalary+0.12*this.basicSalary;
     }
 }
 
 class NeoTrainer extends NeoEmployee{
-    trainingTechnologies:string[]
+    trainingTechnologies:string[];
+    payPerHour:number;
+    extraWorkingHrs:number;
+    // assume trainer getting extrapay for extra hrs of training per month
+     extraPay(){
+        return this.extraWorkingHrs*this.payPerHour;
+    }
+    // new formula for gross salary for trainer
+    // bs+ta+da+hra+ep
+     getGrossSalary():number{
+        console.log("in NeoTrainer method");
+        return this.basicSalary+0.05*this.basicSalary+0.1*this.basicSalary+0.12*this.basicSalary+this.extraPay();
+    } 
+
+}
+
+class SoftSkillTrainer extends NeoTrainer{
+    getGrossSalary():number{
+        return 9999;
+    }
 }
 console.log("------EMPLOYEE---------------");
 
@@ -28,7 +48,12 @@ console.log("------TRAINER---------------");
 let trainer=new NeoTrainer();
 trainer.employeeId=888;
 trainer.employeeName="Mayura";
-trainer.basicSalary=95000;
+trainer.basicSalary=89000;
 trainer.deptId="LD";
 trainer.trainingTechnologies=['JAVA','HTML','Javascript','Bootstrap'];
-console.log(trainer.getGrossSalary()); // super class method
+trainer.payPerHour=1000;
+trainer.extraWorkingHrs=15;
+console.log(trainer.getGrossSalary()); // polymorphism 
+
+
+
