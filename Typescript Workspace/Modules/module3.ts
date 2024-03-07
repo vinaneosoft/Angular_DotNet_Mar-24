b14d6f28061da7415183c308ab36638c11de0f31
kokanevina <kokane.vina@gmail.com>
2024-03-07 14:45:00 +0530

    commit

--- Typescript Workspace/Modules/module3.ts
+++ Typescript Workspace/Modules/module3.ts
@@ -4,13 +4,11 @@ let departmentName="LD";
 let trainerName="Vipul";
 
 let companyName="neosoft";
-
 class ABC{
 
 }
 
-export {departmentName, trainerName, ABC}
-
+export {departmentName, trainerName}
 export default companyName;
 /**A module cannot have multiple default exports */
-
+//export default ABC;
