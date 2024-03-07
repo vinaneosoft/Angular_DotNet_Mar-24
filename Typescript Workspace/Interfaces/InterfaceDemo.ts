6d95e4ae43ade60fb2ec04efeb41f208b013de84
kokanevina <kokane.vina@gmail.com>
2024-03-07 11:44:01 +0530

    commit

--- Typescript Workspace/Interfaces/InterfaceDemo.ts
+++ Typescript Workspace/Interfaces/InterfaceDemo.ts
@@ -1,33 +1,32 @@
 
 interface DeveloperIntf{
     employeeId:number;
-    departmentId:number;
+    departmentId:string;
     showDetails():void;
 }
 class NeoDeveloper implements DeveloperIntf{
     employeeId: number;
-    departmentId:number;
+    departmentId:string;
     showDetails(): void {
       console.log("NeoDeveloper Details");
     }
-
-
 }
-
 class WebWerksDeveloper implements DeveloperIntf{
     employeeId:number;
-    departmentId: number;
+    departmentId: string;
     showDetails(): void {
         console.log("WebWerksDeveloper Details");
     }
 }
 
-
 let developer:DeveloperIntf;
-
 developer=new NeoDeveloper();
+developer.employeeId=111;
+developer.departmentId="JS";
 developer.showDetails();
 
 
 developer=new WebWerksDeveloper();
+developer.employeeId=123;
+developer.departmentId="UIUX"
 developer.showDetails();
