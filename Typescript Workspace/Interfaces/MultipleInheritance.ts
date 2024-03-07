9fb0bcee6d206a1478df0b56c581c05cdbb7b937
kokanevina <kokane.vina@gmail.com>
2024-03-07 12:27:15 +0530

    commit

--- Typescript Workspace/Interfaces/MultipleInheritance.ts
+++ Typescript Workspace/Interfaces/MultipleInheritance.ts
@@ -23,3 +23,74 @@ class Child implements FatherIntf, MotherIntf{
     }
    
 }
+
+interface StackIntf{
+    push(e:number):number;
+    pop():number;
+    display():void
+}
+
+interface QueueIntf{
+    insert(e:number):number;
+    remove():number;
+    display():void
+}
+
+interface DequeInterface extends QueueIntf{
+    insertFront(e:number):number;
+    removeLast():number;
+}
+
+class Deque implements DequeInterface{
+    display(): void {
+       
+    }
+
+    insertFront(e: number): number {
+        return 0
+    }
+    removeLast(): number {
+        return 0
+    }
+    insert(e: number): number {
+        return 0
+    }
+    remove(): number {
+        return 0
+    }
+    //2, 4 ?
+}
+
+// rule : mulitple interfaces can be extended by another interface , if method signature in those
+// interfaces is same then another interface can inherit it one time
+// if method name is same and signature is difft then, error occurs. In this case at a time
+// we can not extend those multiple interfaces
+interface MixIntf extends StackIntf, QueueIntf{
+    // empty
+}
+
+/** class can extend one class
+ * class can implement many interfaces
+ * interface can extend many interfaces
+ */
+
+/** 
+ * class B{}
+ * class A implements B  : wrong
+ * 
+ * 
+ * interface B{}
+ * class A implements B   : right
+ * 
+ * interface C{}
+ *  interface D implements C : wrong
+ * 
+ *  interface E extends B,C  : right
+ * 
+ * class M extends A implements B : right
+ * 
+ * class M implements B extends A : wrong
+ * 
+ * class X{} class Y{}
+ * interface Z extends X,Y   : wrong
+ */
