e345784532be92dbf961cd26758fd3abbe3c9a4b
kokanevina <kokane.vina@gmail.com>
2024-03-07 12:44:22 +0530

    commit

---
+++ Typescript Workspace/Modules/module1.ts
@@ -0,0 +1,27 @@
+// Module : module1
+
+export class BankAccount{
+    accountNumber:number
+    accountBalance:number
+    accountType:string
+    customerId:number;
+    public customerName:string
+  constructor(custName="not set", accountType="savings",balance=0,customerId=0, accNum=0){
+      this.customerName=custName;
+      this.accountNumber=accNum;
+      this.accountBalance=balance;
+      this.accountType=accountType;
+      this.customerId=customerId;
+  }   
+  depositMoney(amount:number):number{
+    this.accountBalance=this.accountBalance+amount;
+    return this.accountBalance;
+  }
+  withdrawMoney(amount:number):number{
+    this.accountBalance=this.accountBalance-amount;
+    return this.accountBalance;
+  }
+}
+
+
+let bankName="Axis";
