8b097e86afb74d3a8d8d79ee40a50f3795101f4c
kokanevina <kokane.vina@gmail.com>
2024-03-07 15:41:47 +0530

    commit

--- Typescript Workspace/Generics/WrapperDemo.js
+++ Typescript Workspace/Generics/WrapperDemo.js
@@ -3,8 +3,19 @@ var s2 = "india"; // string literal
 var s3 = "country india";
 console.log(s1 == s2); // true
 console.log(s1 == s3); // false
-var s4 = new String('Pooja');
+console.log(typeof s1); // string
+var s4 = new String('Pooja'); // Wrapper : String object
 var s5 = new String('Pooja');
+var s6 = new String(s1);
 console.log(s4 == s5); //false
 console.log(s4 === s5); //false
-console.log(s4.valueOf() == s5.valueOf());
+console.log(s4.valueOf() == s5.valueOf()); // true
+console.log(typeof s4); // object
+console.log(s4.toUpperCase()); // POOJA
+console.log(s4); // Pooja
+var n1 = 45;
+var n2 = new Number(n1);
+console.log(n2 instanceof Number);
+var n3;
+n3 = n1; // not Wrapping
+console.log(n3 instanceof Number);
--- Typescript Workspace/Generics/WrapperDemo.ts
+++ Typescript Workspace/Generics/WrapperDemo.ts
@@ -4,14 +4,32 @@ let s2="india"; // string literal
 let s3="country india"
 console.log(s1==s2);// true
 console.log(s1==s3); // false
+console.log(typeof s1); // string
+
 
 let s4=new String('Pooja'); // Wrapper : String object
 let s5=new String('Pooja');
+let s6=new String(s1);
 console.log(s4==s5);//false
 console.log(s4===s5);//false
 console.log(s4.valueOf()==s5.valueOf());// true
+console.log(typeof s4); // object
+
+
+console.log(s4.toUpperCase());// POOJA
+console.log(s4); // Pooja
 
 
 
+let n1=45;
+let n2=new Number(n1);
+console.log(n2 instanceof Number);
 
 
+let n3:Number;
+n3=n1; // not Wrapping
+console.log(n3 instanceof Number);
+
+
+let marriedStatus=true;
+let married=new Boolean(marriedStatus);
