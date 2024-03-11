28368180d1aa75ba3ffc39d08d0fae20449bef66
kokanevina <kokane.vina@gmail.com>
2024-03-11 17:07:14 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/account-card/account-card.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/account-card/account-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BankAccount } from '../classes/bank-account';
 
 
@@ -12,4 +12,18 @@ export class AccountCardComponent {
 
   @Input()// data in customerAccount is input from parent
   customerAccount:BankAccount=new BankAccount();
+
+  accountHeading="ACCOUNT DETAILS" // this heading to be passed to parent from child
+
+  @Output()
+  eventEmitter=new EventEmitter<string>(); //1.
+
+  constructor(){
+    console.log("in constructor of Account Card Component");
+    // logic 
+  }
+  ngOnInit(){
+    console.log("in init method of Account Card Component");
+    this.eventEmitter.emit(this.accountHeading); //2. // via event we are passing data to direct parent
+  }
 }
--- Angular Workspace/bankaccountmanagement/src/app/customer-account/customer-account.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/customer-account/customer-account.component.ts
@@ -9,6 +9,7 @@ export class CustomerAccountComponent {
 
    bankAccounts=new Array<BankAccount>();
 
+   heading="";
   // we r going to manage all bank accounts here
   constructor(){
     this.createAccount();  }
@@ -18,5 +19,9 @@ export class CustomerAccountComponent {
     this.bankAccounts.push(new BankAccount(83000,"current","Radhika Magar",787,86868686,new Date('12 May 2015')));
     this.bankAccounts.push(new BankAccount(123000,"current","Amar Purohit",898,44444444));
     this.bankAccounts.push(new BankAccount(235000,"salary","Sandesh Mane",121,78787878,new Date('15 Feb 2020')));
+  }
+
+  passData(data:string){
+  this.heading=data;  //4.
   }
 }
