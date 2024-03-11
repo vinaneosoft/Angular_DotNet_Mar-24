0f123c051ce419469db885b39a491102b32fc4fe
kokanevina <kokane.vina@gmail.com>
2024-03-11 15:51:36 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/account-card/account-card.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/account-card/account-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BankAccount } from '../classes/bank-account';
+
 
 @Component({
   selector: 'app-account-card',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './account-card.component.css'
 })
 export class AccountCardComponent {
+  // account details???
 
+  @Input()// data in customerAccount is input from parent
+  customerAccount:BankAccount=new BankAccount();
 }
