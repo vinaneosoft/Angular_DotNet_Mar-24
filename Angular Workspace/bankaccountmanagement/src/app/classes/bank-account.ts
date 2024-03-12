1a14a568a5bc40037e922af9c8b58208586e317e
kokanevina <kokane.vina@gmail.com>
2024-03-12 18:27:42 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/classes/bank-account.ts
+++ Angular Workspace/bankaccountmanagement/src/app/classes/bank-account.ts
@@ -2,6 +2,7 @@ export class BankAccount {
     constructor(
         public accountBalance=0,
         public accountType="Savings",
+        public password="ax123",
         public customerName="BBB",
         public customerId=0,
         public id=0,
--- Angular Workspace/bankaccountmanagement/src/app/customer-account/customer-account.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/customer-account/customer-account.component.ts
@@ -20,16 +20,18 @@ export class CustomerAccountComponent {
       accountBalance:new FormControl("", [Validators.required, Validators.min(0)]), //1.
       customerName:new FormControl("",[Validators.required, Validators.pattern("[a-zA-Z ]*"), Validators.minLength(2)]),
       customerId:new FormControl("",[Validators.required]),
-      id:new FormControl("")
+      id:new FormControl(""),
+      password:new FormControl(""),
+      confirmPassword:new FormControl("")
     });
   }
   createAccount(){
-    this.bankAccounts.push(new BankAccount(23000.78655,"current","poonam Shah",666,67676767,new Date('12 March 2000')));
-    this.bankAccounts.push(new BankAccount(13000.4563,"savings","SaMeer pujari",555,55555555,new Date('1 March 2000')));
-    this.bankAccounts.push(new BankAccount(83000.45,"current","Radhika magar",787,86868686,new Date('12 May 2015')));
-    this.bankAccounts.push(new BankAccount(123000,"current","Amar purohit",898,44444444));
-    this.bankAccounts.push(new BankAccount(1235000.123,"salary","SaNdesh Mane",121,78787878,new Date('15 Nov 2020')));
-    this.bankAccounts.push(new BankAccount(1235000.123,"salary","SaNdesh Kane",111,88787878));
+    this.bankAccounts.push(new BankAccount(23000.78655,"current",'x123',"poonam Shah",666,67676767,new Date('12 March 2000')));
+    this.bankAccounts.push(new BankAccount(13000.4563,"savings","x123","SaMeer pujari",555,55555555,new Date('1 March 2000')));
+    this.bankAccounts.push(new BankAccount(83000.45,"current","x123","Radhika magar",787,86868686,new Date('12 May 2015')));
+    this.bankAccounts.push(new BankAccount(123000,"current","x123","Amar purohit",898,44444444));
+    this.bankAccounts.push(new BankAccount(1235000.123,"salary","x123","SaNdesh Mane",121,78787878,new Date('15 Nov 2020')));
+    this.bankAccounts.push(new BankAccount(1235000.123,"salary","x123","SaNdesh Kane",111,88787878));
   }
   setDetails(){
     /* console.log(this.accountForm); */
