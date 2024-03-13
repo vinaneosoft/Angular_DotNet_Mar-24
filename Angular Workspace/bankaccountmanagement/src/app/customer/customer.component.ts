9be8b5f86d590c8b2f0b17790befc2142b7faee4
kokanevina <kokane.vina@gmail.com>
2024-03-13 17:05:14 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/customer/customer.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Customer } from '../classes/customer';
+import { ActivatedRoute } from '@angular/router'; //1.
 
 @Component({
   selector: 'app-customer',
@@ -7,10 +8,11 @@ import { Customer } from '../classes/customer';
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
-
-
   customers:Customer[];
-  constructor(){
+  param:any;
+  customerId=0;
+  constructor(public routeService:ActivatedRoute) // service object automatically injected in constructor of component: DI
+  {
     this.customers=[
       new Customer(666,"poonam Shah", 9090909090, "poonam@gmail.com"),
       new Customer(555,"SaMeer pujari", 8989898989, "pujari@gmail.com"),
@@ -19,6 +21,11 @@ export class CustomerComponent {
       new Customer(121,"SaNdesh Mane", 9898989898, "smane@gmail.com"),
       new Customer(111,"SaNdesh Kane", 9999999999, "skane@gmail.com"),
     ]
+    this.extractRouteParameter();
   }
-  
+  extractRouteParameter(){
+      this.param=this.routeService.snapshot.params['custId'];
+      this.customerId=parseInt(this.param);
+      //console.log(this.customerId);
+  }
 }
