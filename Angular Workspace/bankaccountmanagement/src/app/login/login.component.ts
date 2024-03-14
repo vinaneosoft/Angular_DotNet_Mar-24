b5bf23f30323ecb883ff06c8e150a27bda751111
kokanevina <kokane.vina@gmail.com>
2024-03-14 14:49:03 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/login/login.component.ts
+++ Angular Workspace/bankaccountmanagement/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../myservices/user.service';
+import { Router } from '@angular/router';  //1. 
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,16 @@ import { UserService } from '../myservices/user.service';
 export class LoginComponent {
   flag=false;
   loginError="";
-  constructor(private userService:UserService){}
+  constructor(private userService:UserService, private router:Router){}  //1.
   login(loginForm:any){
+    this.loginError="";
   this.flag=this.userService.loginCheck(loginForm.value.username,loginForm.value.password);
   if(this.flag)
-    {// navigation logic
+    {
+      window.alert("Logged in successfully.......");
+      this.router.navigate(['home']);
     }
   else
       this.loginError="Incorrect username or password"
-    
   }
 }
