0c0ce10028818a5fb41e516795a45fdfa249ba55
kokanevina <kokane.vina@gmail.com>
2024-03-18 15:13:24 +0530

    commit

--- Angular Workspace/bankaccountmanagement/src/app/myservices/user.service.ts
+++ Angular Workspace/bankaccountmanagement/src/app/myservices/user.service.ts
@@ -6,16 +6,15 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class UserService {
 
-  username="admin";
+  username="admin"; // administrator, bankadminstrator1,...2
   password="Admin@123"
   loginFlag=false; 
   constructor(private cookie:CookieService) { }
   loginCheck(username:string, password:string):boolean{
-    // if type is admin : adminLoginCheck
+    // if username includes admin : adminLoginCheck
     //else customerLoginCheck
     return this.adminLoginCheck(username,password);
   }
-
   adminLoginCheck(username:string, password:string):boolean{
     if(username==this.username && password== this.password){
       this.loginFlag=true
@@ -26,7 +25,12 @@ export class UserService {
      return this.loginFlag;
   }
 
-
+  customerLoginCheck(){
+    // get data from json
+    // convert into array
+    // apply find method on array if u get account object then....
+    // set ("customer",username)
+  }
   logOut(){
     this.loginFlag=false;
     this.cookie.delete("bankadmin");
